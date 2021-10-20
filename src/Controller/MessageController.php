cefe6f196e88881719517c6281856ab992579922
Enrique Hernandez <enrique.hernandez@experteam.com.ec>
2021-10-20 16:12:19 -0500

    fix: added v2 for redis data and message controller.

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -7,6 +7,7 @@ use Experteam\ApiBaseBundle\Schemas\FailResponse;
 use Experteam\ApiBaseBundle\Schemas\SuccessResponse;
 use Experteam\ApiCrudBundle\Schemas\MessageInput;
 use Experteam\ApiRedisBundle\Service\RedisTransport\RedisTransportInterface;
+use Experteam\ApiRedisBundle\Service\RedisTransportV2\RedisTransportV2Interface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Nelmio\ApiDocBundle\Annotation\Security;
@@ -35,6 +36,41 @@ class MessageController extends BaseController
      * @return array
      */
     public function new(Request $request, RedisTransportInterface $redisTransport): array
+    {
+        /** @var MessageInput $messageInput */
+        $messageInput = $this->requestUtil->validate($request->getContent(), MessageInput::class);
+
+        if (count($messageInput->entities) > 0) {
+            $namespace = "App\\Entity\\";
+
+            foreach ($messageInput->entities as &$entity) {
+                if (strpos($entity, $namespace) === false) {
+                    $entity = $namespace . $entity;
+                }
+            }
+        }
+
+        $redisTransport->restoreMessages($messageInput->dateTimeFrom, $messageInput->dateTimeTo, $messageInput->entities, $messageInput->entityIds);
+        return [];
+    }
+
+    /**
+     * Creates a Message resource.
+     *
+     * @Security(name="Bearer")
+     *
+     * @OA\RequestBody(@Model(type=MessageInput::class), description="The new Message resource.")
+     *
+     * @Rest\View()
+     * @OA\Response(response=200, @Model(type=SuccessResponse::class), description="Success response.")
+     * @OA\Response(response=400, @Model(type=FailResponse::class), description="Client error response.")
+     * @OA\Response(response=500, @Model(type=ErrorResponse::class), description="Server error response.")
+     *
+     * @param Request $request
+     * @param RedisTransportV2Interface $redisTransport
+     * @return array
+     */
+    public function newV2(Request $request, RedisTransportV2Interface $redisTransport): array
     {
         /** @var MessageInput $messageInput */
         $messageInput = $this->requestUtil->validate($request->getContent(), MessageInput::class);
--- src/Controller/RedisDataController.php
+++ src/Controller/RedisDataController.php
@@ -6,6 +6,7 @@ use Experteam\ApiBaseBundle\Schemas\ErrorResponse;
 use Experteam\ApiBaseBundle\Schemas\FailResponse;
 use Experteam\ApiBaseBundle\Schemas\SuccessResponse;
 use Experteam\ApiRedisBundle\Service\RedisTransport\RedisTransportInterface;
+use Experteam\ApiRedisBundle\Service\RedisTransportV2\RedisTransportV2Interface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Nelmio\ApiDocBundle\Annotation\Security;
@@ -32,6 +33,27 @@ class RedisDataController extends BaseController
      * @return array
      */
     public function new(RedisTransportInterface $redisTransport): array
+    {
+        $redisTransport->restoreData();
+        return [];
+    }
+
+    /**
+     * Creates a Redis data resource.
+     *
+     * @Security(name="Bearer")
+     *
+     * @Rest\Post()
+     *
+     * @Rest\View()
+     * @OA\Response(response=200, @Model(type=SuccessResponse::class), description="Client success response.")
+     * @OA\Response(response=400, @Model(type=FailResponse::class), description="Client error response.")
+     * @OA\Response(response=500, @Model(type=ErrorResponse::class), description="Server error response.")
+     *
+     * @param RedisTransportV2Interface $redisTransport
+     * @return array
+     */
+    public function newV2(RedisTransportV2Interface $redisTransport): array
     {
         $redisTransport->restoreData();
         return [];
