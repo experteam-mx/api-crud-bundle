6070a0fc5d1f95350cf5e31d200932c2c99891f4
Roberlay León Piñero <roberlayl@gmail.com>
2024-03-01 20:22:20 -0500

    Revert "fix(paginator): Correct the total of the paginate function"
    This reverts commit feaefbf6b11f6e58cffb0d223eddf80881ea44b4.

--- src/Service/Paginator/PaginatorInterface.php
+++ src/Service/Paginator/PaginatorInterface.php
@@ -9,11 +9,34 @@ use Symfony\Component\HttpFoundation\Request;
 
 interface PaginatorInterface
 {
+    /**
+     * @param string $collectionKey
+     * @param Request $request
+     * @param ServiceEntityRepository $repository
+     * @param array $criteria
+     * @return array
+     */
     public function paginate(string $collectionKey, Request $request, ServiceEntityRepository $repository, array $criteria = []): array;
 
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param Request $request
+     * @param array $criteria
+     * @return QueryBuilder
+     */
     public function queryBuilderForResult(QueryBuilder $queryBuilder, Request $request, array $criteria = []): QueryBuilder;
 
-    public function queryForTranslatable(QueryBuilder $queryBuilder, Request $request): Query;
-
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param array $criteria
+     * @return QueryBuilder
+     */
     public function queryBuilderForTotal(QueryBuilder $queryBuilder, array $criteria = []): QueryBuilder;
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param Request $request
+     * @return Query
+     */
+    public function queryForTranslatable(QueryBuilder $queryBuilder, Request $request): Query;
 }
